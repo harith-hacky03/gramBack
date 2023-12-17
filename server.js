23ff65d38c6675d0310807136ed4f4de84cd9195
Harith <108193091+harith-hacky03@users.noreply.github.com>
2023-12-17 20:49:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -106,17 +106,7 @@ function frndPosts(user_id)
     })
 }
 
-app.get('/myId',async(req,res)=>{
-    console.log(req.body.username)
-    res.send('eee')
-})
 
-function getmyID(user_name)
-{
-    return new Promise((resolve,reject)=>{
-        let query=`SELECT user_id FROM user_auth WHERE user_name=${user_name}`
-    })
-}
 
 app.get('/uploadPost',async(req,res)=>{
     try{
